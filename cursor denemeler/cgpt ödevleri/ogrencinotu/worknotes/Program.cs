1e46e671f18584bf7496b451773e99270dd7fcf9
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-02-27 23:09:51 +0300

    worknotes

---
+++ cursor denemeler/cgpt ödevleri/ogrencinotu/worknotes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Aldığınız notu giriniz");
+        int not = Convert.ToInt32(Console.ReadLine());
+
+        if (not <= 50)
+        {
+            Console.WriteLine("Sınavdan Kaldınız");
+            return;
+        }
+        if (not >= 80)
+        {
+            Console.WriteLine("Sınavda Yüksek Derece Yaptınız");
+            return;
+
+        }
+        if (not >= 51)
+        {
+            Console.WriteLine("Sınavdan Geçtiniz");
+            return;
+        }
+        
+    }
+
+}
+
