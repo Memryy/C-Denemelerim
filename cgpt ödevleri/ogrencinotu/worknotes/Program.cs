2f625399ff4e54e06771caca750aa49e0fe983d5
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-02-27 23:54:15 +0300

    asd

---
+++ cgpt ödevleri/ogrencinotu/worknotes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Aldığınız notu giriniz:");
+        int not = Convert.ToInt32(Console.ReadLine());
+
+        if (not >= 85)
+        {
+            Console.WriteLine("Sınavda Yüksek Derece Yaptınız");
+        }
+        else if (not >= 50)
+        {
+            Console.WriteLine("Sınavdan Geçtiniz");
+        }
+        else
+        {
+            Console.WriteLine("Sınavdan Kaldınız");
+        }
+    }
+}
