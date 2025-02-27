197ab19fbf9fe9512d2079579c64354d1f3f5835
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-02-27 22:24:23 +0300

    Kosul Yukleme denemesi

---
+++ denemederleme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi > 0)
+        {
+            Console.WriteLine("Sayı Pozitiftir");
+        }
+        else if (sayi < 0)
+        {
+            Console.WriteLine("Sayı Negatiftir");
+        }
+        else
+        {
+            Console.WriteLine("Sayıyı Sıfır Girdiniz");
+        }
+    }
+}
