3e9c99ccd3dbb6b1e1c79293fd6b5a3750db1856
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-02-28 20:02:03 +0300

    listduezeni

---
+++ listduzeni/listduzeni/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> sayılar = new List<int>();
+        
+        sayılar.add(10);
+        sayılar.add(20);
+        sayılar.add(30);
+        sayılar.add(40);
+        sayılar.add(50);
+
+        foreach (int sayı in sayılar)
+        {
+            Console.WriteLine(sayı);
+        }
+        
+    }
+}
