bdeddbce9047d41fd668d65cb4c7fd7ff61794fa
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-03-01 19:25:51 +0300

    arrayy

---
+++ arrayy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string [] alisverislistesi = {"Elma", "Yumurta", "Domates", "Ekmek", "Tavuk", "Tereyağı", "Kola"};
+
+        Console.WriteLine("Alışveriş Listesi");
+        foreach (var urun in alisverislistesi)
+        {
+            Console.WriteLine($"-{urun}");
+        }        
+    }
+}
