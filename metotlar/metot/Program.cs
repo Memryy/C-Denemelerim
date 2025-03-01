0fa4d15eb1d08455f248e9bd42fa830216f71369
Mehmet İnci <mehmetinci@Mehmet-MacBook-Air.local>
2025-03-01 20:24:24 +0300

    metot

---
+++ metotlar/metot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        KullaniciSelamla("Mehmet");
+
+    }
+
+    static void KullaniciSelamla(string isim)
+    {
+        Console.WriteLine($"Merhaba {isim}, hoşgeldin!");
+    }
+}
